9fbb271c65f4264a69b7b14c186a39f9df445320
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-18 18:41:44 -0300

    Added domain entity "Book" and configured auto migration

--- database/migrations/migrations.go
+++ database/migrations/migrations.go
@@ -1,12 +1,13 @@
 package migrations
 
 import (
+	"books-api/model"
 	"gorm.io/gorm"
 	"log"
 )
 
 func RunMigration(db *gorm.DB) {
-	err := db.AutoMigrate()
+	err := db.AutoMigrate(model.Book{})
 
 	if err != nil {
 		log.Fatal("Error: ", err)
---
+++ model/boot.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Book struct {
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	MediumPrice string         `json:"medium_price"`
+	Author      string         `json:"author"`
+	ImageURL    string         `json:"image_url"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+}
